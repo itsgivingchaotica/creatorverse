2606a94cbb618f2284f11eba698b3caf0868fe53
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-19 18:39:55 -0400

    fetch creators and wrap app with creators context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   createHashRouter,
   createRoutesFromElements,
@@ -6,6 +7,8 @@ import {
 } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import { AddCreator, EditCreator, ShowCreators, ViewCreator } from "./pages";
+import { supabase } from "./client";
+import { CreatorsContext } from "./CreatorsContext";
 
 const router = createHashRouter(
   createRoutesFromElements(
@@ -19,11 +22,31 @@ const router = createHashRouter(
 );
 
 function App() {
+  const [creators, setCreators] = useState([]);
+
+  useEffect(() => {
+    const fetchCreators = async () => {
+      try {
+        const { data, error } = await supabase.from("creators").select();
+        setCreators(data);
+        console.log(data);
+        if (error) {
+          console.log(error);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchCreators();
+  }, []);
+
   return (
     <>
-      <div className="App">
-        <RouterProvider router={router} />
-      </div>
+      <CreatorsContext.Provider value={{ creators, setCreators }}>
+        <div className="App">
+          <RouterProvider router={router} />
+        </div>
+      </CreatorsContext.Provider>
     </>
   );
 }
