b436683417d7d1b828be971fb0002a47a93575ee
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-21 03:12:28 -0400

    Update client.js
    place env variable

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const URL = "https://qoidluxqxscyqmnbxfho.supabase.co";
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvaWRsdXhxeHNjeXFtbmJ4ZmhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI0MjQyMjcsImV4cCI6MjAwODAwMDIyN30.NqtD-K80f9HVdZ5L-UfZ4Wc5ojfYvfSMKgZa9EEkasw";
+const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
 
 export const supabase = createClient(URL, API_KEY);
