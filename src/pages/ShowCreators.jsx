c3d243fdf7517afdc2df2fa283a4b651d7248853
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-21 02:42:01 -0400

    allows realtime edits

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,10 +1,63 @@
 import React, { useContext, useEffect } from "react";
 import Creator from "../components/Creator";
 import { CreatorsContext } from "../CreatorsContext";
+import { supabase } from "../client";
 import "../styles/creator.css";
 
 const ShowCreators = () => {
-  let { creators } = useContext(CreatorsContext);
+  let { creators, setCreators } = useContext(CreatorsContext);
+
+  useEffect(() => {
+    //fetch the creators to provide updates by getting table from database
+    const fetchCreators = async () => {
+      try {
+        const { data, error } = await supabase.from("creators").select();
+        setCreators(data);
+        console.log(data);
+        if (error) {
+          console.log(error);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    //create real time update channel
+    const channel = supabase
+      .channel("schema-db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "creators",
+        },
+        (payload) => {
+          setCreators((prevCreators) => {
+            const updatedCreatorIndex = prevCreators.findIndex(
+              (creator) => creator.id === payload.new.id
+            );
+            // Update the local state with the new creator
+            if (updatedCreatorIndex !== -1) {
+              const updatedCreators = [...prevCreators];
+              updatedCreators[updatedCreatorIndex] = payload.new;
+              return updatedCreators;
+            } else {
+              //update with the new creator adde
+              return [...prevCreators, payload.new];
+            }
+          });
+        }
+      )
+      .subscribe();
+
+    fetchCreators();
+
+    return () => {
+      // Clean up subscription on component unmount
+      channel.unsubscribe();
+    };
+  }, []);
 
   return (
     <div className="creators-list">
